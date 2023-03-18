578264fdd96f7ced40e462436858004096f18430
Louis Suo <louis.suo@myob.com>
2023-03-18 12:48:58 +1000

    Initial Commit

---
+++ playAI/playAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  playAIApp.swift
+//  playAI
+//
+//  Created by Louis Suo on 18/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct playAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
